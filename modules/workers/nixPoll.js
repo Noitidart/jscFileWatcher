b00938a49284cbaafdbf3083f7e2bb67faa3c82a
P0lip <P0lip@users.noreply.github.com>
2015-04-10 00:21:08 +0200

    initial.rev47

--- modules/workers/nixPoll.js
+++ modules/workers/nixPoll.js
@@ -19,34 +19,35 @@ var cOS = 'linux';
 self.onmessage = function (msg) {
 	// msg must be fd
 	pollThis(msg.data, true);
-}
+};
 
 
 function pollThis(fd, restartAfterChange) {
 	console.log('ok in pollThis of nixPoll');
-		  var count = ostypes.TYPE.inotify_event.size; //size_t
-		  var buf = ctypes.ArrayType(ostypes.TYPE.char, count)(); // docs page here http://linux.die.net/man/7/inotify says sizeof(struct inotify_event) + NAME_MAX + 1 will be sufficient to read at least one event.
-		  console.log('starting the loop, fd:', fd, 'count:', count);
-		  var length;
-      while (true) {
-		length = ostypes.API('read')(fd, buf, count);
-		if (cutils.jscEqual(length, -1)) {
-			console.error('read failed with -1 and errno: ' + ctypes.errno);
-			throw new Error('read failed with -1 and errno: ' + ctypes.errno);
-		} else if (!cutils.jscEqual(length, 0)) {
-			// then its > 0 as its not -1
-			// something happend, read struct
-		  var casted = ctypes.cast(buf.addressOfElement(0), ostypes.TYPE.inotify_event.ptr).contents;
-		  console.log('casted:', casted.toString());
-		  
-		  var file_name = casted.addressOfField('name').contents.readString();
-		  console.info('file_name:', file_name);
-			self.postMessage('change found');
-			if (!restartAfterChange) {
-				break;
-			}
+    var count = ostypes.TYPE.inotify_event.size; //size_t
+    var buf = ctypes.ArrayType(ostypes.TYPE.char, count)(); // docs page here http://linux.die.net/man/7/inotify says sizeof(struct inotify_event) + NAME_MAX + 1 will be sufficient to read at least one event.
+    console.log('starting the loop, fd:', fd, 'count:', count);
+    var length = ostypes.API('read')(fd, buf, count);
+    if (cutils.jscEqual(length, -1)) {
+      console.error('read failed with -1 and errno: ' + ctypes.errno);
+      throw new Error('read failed with -1 and errno: ' + ctypes.errno);
+    } else if (!cutils.jscEqual(length, 0)) {
+      // then its > 0 as its not -1
+      // something happend, read struc
+      let i = 0;
+      while (i < length) {
+		let casted = ctypes.cast(buf.addressOfElement(i), ostypes.TYPE.inotify_event.ptr).contents;
+		console.log('casted:', casted.toString());
+		let file_name = casted.addressOfField('name').contents.readString();
+		console.info('file_name:', file_name);
+        i += count + (+casted.addressOfField('name').contents.readString());
+		self.postMessage('change found');
+		if (!restartAfterChange) {
+		  break;
 		}
       }
+    }
+    pollThis(fd);
 	  // maybe close fd here? not sure if we need to if terminate
 	  console.log('ok loop done of pollThis of nixPoll');
 }
