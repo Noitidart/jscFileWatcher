19e9592a7608a6a93bd70be1b86db9ba902e6cff
P0lip <P0lip@users.noreply.github.com>
2015-04-09 14:40:50 +0200

    inital.rev37

--- modules/workers/nixPoll.js
+++ modules/workers/nixPoll.js
@@ -1,3 +1,4 @@
+/*jshint node:true, esnext: true, moz: true, worker: true*/
 'use strict';
 importScripts('resource://gre/modules/workers/require.js');
 
@@ -14,42 +15,25 @@ var core = {
 importScripts(core.path.chrome + 'modules/cutils.jsm');
 importScripts(core.path.chrome + 'modules/ostypes_nix.jsm');
 
-var cOS = 'linux';
 
-self.onmessage = function (msg) {
-	// msg must be fd
-	pollThis(msg.data);
-}
+self.onmessage = msg => pollThis(msg.data);
 
-
-function pollThis(fd, restartAfterChange) {
-	console.log('ok in pollThis of nixPoll');
-	var count = 1024 + ostypes.TYPE.inotify_event.size; //size_t
-	var buf = ctypes.char.array(count)();
-      var i = 0;
-      console.log('starting the loop');
-      while (true) {
-      		i++;
-      		if (i == 100) {
-      			console.log('got to i 100');
-      		}
-      		if (i == 200) {
-      			console.log('got to i 200');
-      		}
-		var length = ostypes.API('read')(fd, buf.address(), count);
-		console.info('length:', length, length.toString())
-		if (length == -1) {
-			throw new Error('read failed');
-		} else if (length > 0) {
-			// something happend, read struct
-			//var casted = ctypes.cast(buf.addressOfElement(0), ostypes.TYPE.inotify_event.ptr).contents;
-			//var fname = casted.addressOfField('fname').readString();
-			console.info('casted:'/*, casted.toString()*/);
-			self.postMessage('change found');
-			if (!restartAfterChange) {
-				break;
-			}
-		}
-      }
-	  console.log('ok loop done of pollThis of nixPoll');
+function pollThis(fd) { // we must an option to stop the loop!
+  var count = 1024 * ostypes.TYPE.inotify_event.size; //size_t
+  var buf = ctypes.char.array(count)();
+  var length = ostypes.API('read')(fd, buf.address(), count);
+  //var changes = new Set(); // Look L34
+  if (length === -1) {
+    self.postMessage('error');
+    throw new Error('read failed');
+  } else {
+    let i = 0;
+    while (i < length) {
+      let casted = ctypes.cast(buf.addressOfElement(0), ostypes.TYPE.inotify_event.ptr).contents; // Dunno how to get a field "len"
+      i += ostypes.TYPE.inotify_event.size; // its compulsory to have this field len to sum up the stuff
+      self.postMessage('change found', casted); // I dont know whether we want to trigger a callback with each time or send a set with changes. What do you think is better?
+    }
+    console.log('ok loop done of pollThis of nixPoll. Starting a new loop');
+    pollThis(fd);
+  }
 }
