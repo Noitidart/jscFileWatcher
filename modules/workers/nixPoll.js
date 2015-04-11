eadba5f23ae1ea187ddbf54ca1f5382b3b1cc71d
P0lip <P0lip@users.noreply.github.com>
2015-04-11 11:34:47 +0200

    initial.rev49

--- modules/workers/nixPoll.js
+++ modules/workers/nixPoll.js
@@ -1,3 +1,4 @@
+/*jshint esnext: true, moz: true, worker: true, node: true*/
 'use strict';
 importScripts('resource://gre/modules/workers/require.js');
 
@@ -10,7 +11,20 @@ var core = {
   },
   aData: 0
 };
-
+const masks = {
+  1: "IN_ACCESS" ,
+  2: "IN_MODIFY",
+  4: "IN_ATTRIB",
+  8: "IN_CLOSE_WRITE",
+  16: "IN_CLOSE_NOWRITE",
+  32: "IN_OPEN",
+  64: "IN_MOVED_FROM",
+  128: "IN_MOVED_TO",
+  256: "IN_CREATE",
+  512: "IN_DELETE",
+  1024: "IN_DELETE_SELF",
+  2048: "IN_MOVE_SELF"
+};
 importScripts(core.path.chrome + 'modules/cutils.jsm');
 importScripts(core.path.chrome + 'modules/ostypes_nix.jsm');
 
@@ -18,18 +32,17 @@ var cOS = 'linux';
 
 self.onmessage = function (msg) {
 	// msg must be fd
-	pollThis(msg.data, true);
+	pollThis(msg.data);
 };
 
 
-function pollThis(fd, restartAfterChange) {
+function pollThis(fd) {
 	console.log('ok in pollThis of nixPoll');
     var count = ostypes.TYPE.inotify_event.size; //size_t
     var buf = ctypes.ArrayType(ostypes.TYPE.char, count)(); // docs page here http://linux.die.net/man/7/inotify says sizeof(struct inotify_event) + NAME_MAX + 1 will be sufficient to read at least one event.
     console.log('starting the loop, fd:', fd, 'count:', count);
     var length = ostypes.API('read')(fd, buf, count);
     if (cutils.jscEqual(length, -1)) {
-      console.error('read failed with -1 and errno: ' + ctypes.errno);
       throw new Error('read failed with -1 and errno: ' + ctypes.errno);
     } else if (!cutils.jscEqual(length, 0)) {
       // then its > 0 as its not -1
@@ -38,16 +51,19 @@ function pollThis(fd, restartAfterChange) {
       while (i < length) {
 		let casted = ctypes.cast(buf.addressOfElement(i), ostypes.TYPE.inotify_event.ptr).contents;
 		console.log('casted:', casted.toString());
-		let file_name = casted.addressOfField('name').contents.readString();
-		console.info('file_name:', file_name);
-        i += count + (+casted.addressOfField('len').contents.readString());
-		self.postMessage('change found');
-		if (!restartAfterChange) {
-		  break;
-		}
+		let fileName = casted.addressOfField('name').contents.readString();
+		let mask = casted.addressOfField('mask').contents;
+		let len = casted.addressOfField('len').contents;
+        let cookie = casted.addressOfField('cookie').contents;
+        console.info('file_name:', fileName);
+        console.info('type:', masks[mask]);
+        i = ++len;
+		self.postMessage('success', {
+          fileName: fileName,
+          type: masks[mask]
+        });
       }
     }
-    pollThis(fd);
-	  // maybe close fd here? not sure if we need to if terminate
-	  console.log('ok loop done of pollThis of nixPoll');
+    //pollThis(fd);
+    console.log('ok loop done of pollThis of nixPoll');
 }
