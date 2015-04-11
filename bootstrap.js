6426f9c87d133447a56c5fa9d8f645457812854d
Noitidart <noitidart@gmail.com>
2015-04-11 16:28:36 -0700

    initial.rev57
    commit to save
    thinking out termination and callback handling

--- bootstrap.js
+++ bootstrap.js
@@ -157,7 +157,17 @@ function mainUsingAPI() {
 }
 
 // start - OS.File.Watcher API
+var FSInitWorker;
+var _FSInitWorker_start_already_triggered = false;
+function _FSInitWorker_start() {
+	// returns promise
+	
+	if (!FSInitWorker) {
+		FSInitWorker = new PromiseWorker(core.path.chrome + 'modules/workers/FSInitWorker.js');
+	}
+}
 var _Watcher_nextId = 0; // never decrement this
+var _Watcher_UnterminatedFSPollWorkers = {}; // used for termination on shutdown // key is aWatcherId and value is the FSPollWorker reference
 function Watcher(aCallback, options = {}) {
 	// returns prototype object, meaning whenever this function is called it should be called like `let watcher = new OS.File.Watcher(callback)`
 	// if user wants to know if it was succesfully initalized they can do this:
@@ -166,61 +176,86 @@ function Watcher(aCallback, options = {}) {
 		watcher.promise_initialized.then(onFulfill, onReject).catch(onCatch);
 	*/
 	// dev user can do watcher.addPath/watcher.removePath before waiting for promise_initalized, as they return promises, those promise will just return after execution after initialization
-	
+	var thisW = this;
 	if (!aCallback || Object.toString.call(aCallback) != '[object Function]') {
 		throw new Error('The argument aCallback is not a function. It must be a function, which optionally takes two arguments: first is aOSPath and second is aEvent.');
 	}
 	
-	this.id = _Watcher_nextId;
+	thisW.id = _Watcher_nextId;
 	_Watcher_nextId++; // so its available to next one
 	
-	this.readState = 0;
+	thisW.readState = 0;
 	// readyState's
 		// 0 - uninintialized
 		// 1 - initialized, ready to do addPaths // when i change readyState to 1, i should check if any paths to add are in queue
 		// 2 - closed due to user calling Watcher.prototype.close
 		// 3 - closed due to failed to initialize
 	
-	this.paths_watched = []; // array of lower cased OS paths, these are inputed user passing a os path to addWatch, that are being watched
+	thisW.cb = aCallback;
 	
-	this.pendingAdds = {}; // object with key aOSPath.toLowerCase()
+	thisW.paths_watched = []; // array of lower cased OS paths, these are inputed user passing a os path to addWatch, that are being watched
+	
+	thisW.pendingAdds = {}; // object with key aOSPath.toLowerCase()
 	
 	var deferred_initialized = new Deferred();
-	this.promise_initialized = deferred_initialized.promise;
+	thisW.promise_initialized = deferred_initialized.promise;
 	
-	var promise_initWatcher = myWorker.post('initWatcher', [this.id]);
-	promise_initWatcher.then(
+	var do_initWatcher = function() {
+		var promise_initWatcher = FSInitWorker.post('initWatcher', [thisW.id]);
+		promise_initWatcher.then(
+		  function(aVal) {
+			console.log('Fullfilled - promise_initWatcher - ', aVal);
+			// start - do stuff here - promise_initWatcher
+			thisW.readState = 1;
+			deferred_initialized.resolve(true);
+			// add in the paths that are waiting
+			for (var pendingAdd in thisW.pendingAdds) {
+				thisW.pendingAdds[pendingAdd]();
+			}
+			// end - do stuff here - promise_initWatcher
+		  },
+		  function(aReason) {
+			var rejObj = {name:'promise_initWatcher', aReason:aReason};
+			console.warn('Rejected - promise_initWatcher - ', rejObj);
+			thisW.readState = 3;
+			deferred_initialized.reject(rejObj);
+			// run through the waiting adds, they are functions which will reject the pending deferred's with .message saying "closed due to readyState 3" as initialization failed
+			for (var pendingAdd in thisW.pendingAdds) {
+				thisW.pendingAdds[pendingAdd]();
+			}
+		  }
+		).catch(
+		  function(aCaught) {
+			var rejObj = {name:'promise_initWatcher', aCaught:aCaught};
+			console.error('Caught - promise_initWatcher - ', rejObj);
+			thisW.readState = 3;
+			deferred_initialized.reject(rejObj);
+			// run through the waiting adds, they are functions which will reject the pending deferred's with .message saying "closed due to readyState 3" as initialization failed
+			for (var pendingAdd in thisW.pendingAdds) {
+				thisW.pendingAdds[pendingAdd]();
+			}
+		  }
+		);
+	};
+	
+	var promise_ensureFSInitWorkerStarted = _FSInitWorker_start();
+	promise_ensureFSInitWorkerStarted.then(
 	  function(aVal) {
-		console.log('Fullfilled - promise_initWatcher - ', aVal);
-		// start - do stuff here - promise_initWatcher
-		this.readState = 1;
-		this.promise_initialized.resolve(true);
-		// add in the paths that are waiting
-		for (var pendingAdd in this.pendingAdds) {
-			this.pendingAdds[pendingAdd]();
-		}
-		// end - do stuff here - promise_initWatcher
+		console.log('Fullfilled - promise_ensureFSInitWorkerStarted - ', aVal);
+		// start - do stuff here - promise_ensureFSInitWorkerStarted
+		do_initWatcher();
+		// end - do stuff here - promise_ensureFSInitWorkerStarted
 	  },
 	  function(aReason) {
-		var rejObj = {name:'promise_initWatcher', aReason:aReason};
-		console.warn('Rejected - promise_initWatcher - ', rejObj);
-		this.readState = 3;
-		this.promise_initialized.reject(rejObj);
-		// run through the waiting adds, they are functions which will reject the pending deferred's with .message saying "closed due to readyState 3" as initialization failed
-		for (var pendingAdd in this.pendingAdds) {
-			this.pendingAdds[pendingAdd]();
-		}
+		var rejObj = {name:'promise_ensureFSInitWorkerStarted', aReason:aReason};
+		console.warn('Rejected - promise_ensureFSInitWorkerStarted - ', rejObj);
+		deferred_initialized.reject(rejObj);
 	  }
 	).catch(
 	  function(aCaught) {
-		var rejObj = {name:'promise_initWatcher', aCaught:aCaught};
-		console.error('Caught - promise_initWatcher - ', rejObj);
-		this.readState = 3;
-		this.promise_initialized.reject(rejObj);
-		// run through the waiting adds, they are functions which will reject the pending deferred's with .message saying "closed due to readyState 3" as initialization failed
-		for (var pendingAdd in this.pendingAdds) {
-			this.pendingAdds[pendingAdd]();
-		}
+		var rejObj = {name:'promise_ensureFSInitWorkerStarted', aCaught:aCaught};
+		console.error('Caught - promise_ensureFSInitWorkerStarted - ', rejObj);
+		deferred_initialized.reject(rejObj);
 	  }
 	);
 	
@@ -234,20 +269,22 @@ Watcher.prototype.addPath = function(aOSPath) {
 	
 	var aOSPathLower = aOSPath.toLowerCase();
 	
+	var thisW = this;
+	
 	var do_addPath = function() {
-		if (this.readState != 1) {
+		if (thisW.readState != 1) {
 			// closed either to failed initialization or user called watcher.close
 			deferredMain_Watcher_addPath.reject({
 				name: 'watcher-closed',
-				message: 'Cannot add as this Watcher was previously closed with reason ' + this.readState
+				message: 'Cannot add as this Watcher was previously closed with reason ' + thisW.readState
 			});
 		} else {
-			var promise_addPath = myWorker.post('addPathToWatcher', [this.id, aOSPath]);
+			var promise_addPath = myWorker.post('addPathToWatcher', [thisW.id, aOSPath]);
 			promise_addPath.then(
 			  function(aVal) {
 				console.log('Fullfilled - promise_addPath - ', aVal);
 				// start - do stuff here - promise_addPath
-				this.paths_watched.push(aOSPathLower);
+				thisW.paths_watched.push(aOSPathLower);
 				deferredMain_Watcher_addPath.resolve(true);
 				// end - do stuff here - promise_addPath
 			  },
@@ -266,19 +303,19 @@ Watcher.prototype.addPath = function(aOSPath) {
 		}
 	};
 	
-	if (this.readyState === 0) {
+	if (thisW.readyState === 0) {
 		// watcher not yet initalized
-		if (aOSPathLower in this.pathsPendingAdd) {
+		if (aOSPathLower in thisW.pathsPendingAdd) {
 			deferredMain_Watcher_addPath.reject({
 				name: 'duplicate-path',
 				message: 'This path is already waiting to be added. It is waiting as the Watcher has not been initailized yet.'
 			});
 		} else {
-			this.pendingAdds[aOSPathLower] = do_addPath;
+			thisW.pendingAdds[aOSPathLower] = do_addPath;
 		}
-	} else if (this.readyState == 1) {
+	} else if (thisW.readyState == 1) {
 		// watcher is ready
-		if (this.paths_watched.indexOf(aOSPathLower) > -1) {
+		if (thisW.paths_watched.indexOf(aOSPathLower) > -1) {
 			deferredMain_Watcher_addPath.reject({
 				name: 'duplicate-path',
 				message: 'This path was already succesfully added by a previous call to Watcher.prototype.addPath.'
@@ -290,7 +327,7 @@ Watcher.prototype.addPath = function(aOSPath) {
 		// watcher is closed
 		deferredMain_Watcher_addPath.reject({
 			name: 'watcher-closed',
-			message: 'Cannot add as this Watcher was previously closed with reason ' + this.readState
+			message: 'Cannot add as this Watcher was previously closed with reason ' + thisW.readState
 		});
 	}
 	
@@ -301,12 +338,13 @@ Watcher.prototype.removePath = function() {
 		// resolves to true if sucesfully removed
 	var deferredMain_Watcher_removePath = new Deferred();
 	
+	var thisW = this;
 	var aOSPathLower = aOSPath.toLowerCase();
 	
-	if (this.readyState === 0) {
+	if (thisW.readyState === 0) {
 		// watcher not yet initalized
-		if (aOSPathLower in this.pathsPendingAdd) {
-			delete this.pathsPendingAdd[aOSPathLower];
+		if (aOSPathLower in thisW.pathsPendingAdd) {
+			delete thisW.pathsPendingAdd[aOSPathLower];
 			deferredMain_Watcher_removePath.resolve(true);
 		} else {
 			deferredMain_Watcher_removePath.reject({
@@ -314,15 +352,15 @@ Watcher.prototype.removePath = function() {
 				message: 'This path was never added, it was not found in watched paths arrays/objects.'
 			});
 		}
-	} else if (this.readyState == 1) {
+	} else if (thisW.readyState == 1) {
 		// watcher is ready
-		if (this.paths_watched.indexOf(aOSPathLower) > -1) {
-			var promise_removePath = myWorker.post('removePathFromWatcher', [this.id, aOSPath]);
+		if (thisW.paths_watched.indexOf(aOSPathLower) > -1) {
+			var promise_removePath = myWorker.post('removePathFromWatcher', [thisW.id, aOSPath]);
 			promise_removePath.then(
 			  function(aVal) {
 				console.log('Fullfilled - promise_removePath - ', aVal);
 				// start - do stuff here - promise_removePath
-				this.paths_watched.splice(this.paths_watched.indexOf(aOSPathLower), 1);
+				thisW.paths_watched.splice(thisW.paths_watched.indexOf(aOSPathLower), 1);
 				deferredMain_Watcher_removePath.resolve(true);
 				// end - do stuff here - promise_removePath
 			  },
@@ -348,7 +386,7 @@ Watcher.prototype.removePath = function() {
 		// watcher is closed
 		deferredMain_Watcher_removePath.reject({
 			name: 'watcher-closed',
-			message: 'No need to remove paths as this Watcher was previously closed with reason ' + this.readState
+			message: 'No need to remove paths as this Watcher was previously closed with reason ' + thisW.readState
 		});
 	}
 	
@@ -358,19 +396,21 @@ Watcher.prototype.close = function() {
 	// returns promise as it has to make a c call
 		// resolves to true on success, else an object explaining why it failed
 	var deferredMain_Watcher_close = new Deferred();
+	var thisW = this;
 	
-	if (this.readyState == 2 || this.readyState == 3) {
+	if (thisW.readyState == 2 || thisW.readyState == 3) {
 		// was already previously closed
 		deferredMain_Watcher_close.reject({
 			name: 'watcher-closed',
-			message: 'Cannot close because Watcher was already previously closed with reason ' + this.readState
+			message: 'Cannot close because Watcher was already previously closed with reason ' + thisW.readState
 		});
 	} else {
-		var promise_closeWatcher = myWorker.post('removePathFromWatcher', [this.id, aOSPath]);
+		var promise_closeWatcher = myWorker.post('removePathFromWatcher', [thisW.id, aOSPath]);
 		promise_closeWatcher.then(
 		  function(aVal) {
 			console.log('Fullfilled - promise_closeWatcher - ', aVal);
 			// start - do stuff here - promise_closeWatcher
+			delete _Watcher_UnterminatedFSPollWorkers[thisW.id];
 			deferredMain_Watcher_close.resolve(true);
 			// end - do stuff here - promise_closeWatcher
 		  },
@@ -389,6 +429,20 @@ Watcher.prototype.close = function() {
 	}
 	
 	return deferredMain_Watcher_close.promise;
+}
+var FSPollWorker;
+Watcher.prototype.waitForNextChange = function() {
+	// returns promise
+	
+	var thisW = this;
+	if (!thisW.FSPollWorker) {
+		thisW.FSPollWorker = new PromiseWorker(core.path.chrome + 'modules/workers/FSPollWorker.js');
+	}
+	
+	
+	// for winnt, if ReadDirectoryChangesW only supports one path per, this func should do a promise for each path to FSPollWorker. a FSPollWorker must be created for each callback as when want to close, to abort all of the promises i can terminate the worker, then itterate through the promises and reject them for reason of Watcher closed.
+	// actually i think for all OS'es each callback should get its own FSPollWorker
+	// question to p0lip: regrading kqueue and inotify, if currently in a poll loop, if addPath do i have to abort the poll and restart it? or will the pre-existing poll also now trigger when the new added path change happens? because this is for case where pre-existing poll is watching dir1, and then addPath(dir2) and then change in dir2 happens, will the prexisting poll fire?
 }
 // end - OS.File.Watcher API
 
@@ -413,7 +467,17 @@ function startup(aData, aReason) {
 }
  
 function shutdown(aData, aReason) {
+	// must terminate workers as they are seperate threads
+	if (FSInitWorker) {
+		FSInitWorker.terminate();
+		// terminate all FSPollWorker's - can do this inside this `if (FSInitWorker)` block because obviously if that doesnt exist then no FSPollWorker's exist
+		for (var aWatcherId in _Watcher_UnterminatedFSPollWorkers) {
+			_Watcher_UnterminatedFSPollWorkers[aWatcherId].terminate();
+		}
+	}
+	
 	if (aReason == APP_SHUTDOWN) { return }
+	
 	Cu.unload(core.path.chrome + 'modules/PromiseWorker.jsm');
 }
 
